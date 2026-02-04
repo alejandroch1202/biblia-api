f2e83c965a1d9889318f9884f0b6db333d91c3eb
alejandroch1202 <alejandrochavez1202@gmail.com>
2026-02-04 17:03:05 -0400

    Pausing due high consumption

--- dist/index.js
+++ dist/index.js
@@ -6,14 +6,14 @@ var _a;
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 require("dotenv/config");
-const router_1 = __importDefault(require("./router"));
-const errors_1 = require("./errors");
+// import router from './router'
+// import { errorLogger, errorHandler, boomErrorHandler } from './errors'
 const app = (0, express_1.default)();
 const PORT = (_a = process.env.PORT) !== null && _a !== void 0 ? _a : 3000;
-(0, router_1.default)(app);
-app.use(errors_1.errorLogger);
-app.use(errors_1.boomErrorHandler);
-app.use(errors_1.errorHandler);
+// router(app)
+// app.use(errorLogger)
+// app.use(boomErrorHandler)
+// app.use(errorHandler)
 app.listen(PORT, () => {
     console.log(`[server] Running API on http://localhost:${PORT}`);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
 import express from 'express'
 import 'dotenv/config'
 // import router from './router'
-import { errorLogger, errorHandler, boomErrorHandler } from './errors'
+// import { errorLogger, errorHandler, boomErrorHandler } from './errors'
 
 const app = express()
 const PORT = process.env.PORT ?? 3000
 
 // router(app)
-app.use(errorLogger)
-app.use(boomErrorHandler)
-app.use(errorHandler)
+// app.use(errorLogger)
+// app.use(boomErrorHandler)
+// app.use(errorHandler)
 
 app.listen(PORT, () => {
   console.log(`[server] Running API on http://localhost:${PORT}`)
